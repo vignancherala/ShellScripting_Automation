c3c2a917a4643e613d19778ed1fe8bd921e6508a
vignancherala <144933621+vignancherala@users.noreply.github.com>
2023-12-05 13:56:26 -0500

    Update automation2.sh

--- automation2.sh
+++ automation2.sh
@@ -2,5 +2,5 @@
 version="1.2.3.4"
 for line in $(cat jiraid.txt)
 do
-curl -X PUT -u "cheralavignan06@gmail.com:ATATT3xFfGF0c6BC-9oykaRcpQBfNIxgrrHNXTXVlHF8lTWpqkY9raX9bQ2D5zi0aCU6XEQQgV5CJy-aZruzxYsRYZuA2SWvAHhTINSZ4qXvRZo2iwcKDOPVlMgZ6HwBeCzLkVf_G0c-4eaCWq7IOcyec4nNDuJsh8CEB77fg_d9P_qwpxcAzsw=559BFD0F" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://surya1992.atlassian.net/rest/api/3/issue/AL-1
+curl -X PUT -u "cheralavignan06@gmail.com:ATATT3xFfGF0c6BC-9oykaRcpQBfNIxgrrHNXTXVlHF8lTWpqkY9raX9bQ2D5zi0aCU6XEQQgV5CJy-aZruzxYsRYZuA2SWvAHhTINSZ4qXvRZo2iwcKDOPVlMgZ6HwBeCzLkVf_G0c-4eaCWq7IOcyec4nNDuJsh8CEB77fg_d9P_qwpxcAzsw=559BFD0F" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://surya1992.atlassian.net/browse/AL-1
 done 
