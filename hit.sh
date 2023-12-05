56b56d13617ec74e76d0b998f76e3655af7794ad
vignancherala <144933621+vignancherala@users.noreply.github.com>
2023-12-05 15:01:25 -0500

    Update hit.sh

--- hit.sh
+++ hit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl  -X PUT -u "singampallipraveenkumar@gmail.com:XkO2YJUvaof4fsvLAqtM63EA"--data '{"update":{"labels":[{"add":"DEMO"}]}}' --header "Content-Type: application/json" "Authorization: Basic XkO2YJUvaof4fsvLAqtM63EA" https://singam.atlassian.net/rest/api/3/issue/SA-1
+curl -X PUT -u "cheralavignan06@gmail.com:ATATT3xFfGF0c6BC-9oykaRcpQBfNIxgrrHNXTXVlHF8lTWpqkY9raX9bQ2D5zi0aCU6XEQQgV5CJy-aZruzxYsRYZuA2SWvAHhTINSZ4qXvRZo2iwcKDOPVlMgZ6HwBeCzLkVf_G0c-4eaCWq7IOcyec4nNDuJsh8CEB77fg_d9P_qwpxcAzsw=559BFD0F" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://surya1992.atlassian.net/browse/AL-1
 
